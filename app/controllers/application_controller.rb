c73fe2f9bf493946d52f2326f68b5feea04a4c65
yaketa-cq <y.aketa@caraquri.com>
2021-06-28 16:15:08 +0900

    JWTを用いたログイン、サインアップ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+  include JwtAuthenticator
+  rescue_from RuntimeError, with: :authorization_error
+  rescue_from Exception, with: :imgur_error
+
+  def render_json_delete(status, message)
+    render json: { message: message }, status: status
+  end
+
+  def render_json_jwt_success(jwt)
+    render json: { jwt: jwt }, status: 200
+  end
+
+  def render_json_jwt_invalid(jwt)
+    render json: { jwt: jwt }, status: 401
+  end
+
+  def authorization_error(jwt_error)
+    render json: { message: jwt_error.message }, status: 401
+  end
+
+  def render_json_jwt_error
+    render json: { message: I18n.t("message.invalid_authorization") }, status: 401
+  end
 end
